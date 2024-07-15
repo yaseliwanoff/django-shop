89548f09d2f35aeddf2edef9142c8cd686897bdd
miro <144848251+yaseliwanoff@users.noreply.github.com>
2024-07-15 16:39:32 +0300

    reload urls

--- advanced_project/urls.py
+++ advanced_project/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop', include('shop.urls', namespace='shop')),
+    path('shop/', include('shop.urls', namespace='shop')),
 ]
 
 if settings.DEBUG:
